47147c041050f8cb341c63a6c4774846db331efa
bodynar <bodynar@gmail.com>
2023-02-15 23:56:05 +0300

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-export * from './components/types';
-export * from './components/anchor';
-export * from './components/button';
-export * from './components/dropdown';
-export * from './components/icon';
-export * from './components/primitives';
-export * from './components/search';
-export * from './components/types';
-export * from './hooks';
+export * from "./components/types";
+
+export * from "./components/anchor";
+export * from "./components/anchor/types";
+
+export * from "./components/button";
+export * from "./components/button/types";
+
+export * from "./components/dropdown";
+export * from "./components/dropdown/types";
+
+export * from "./components/icon";
+
+export * from "./components/primitives";
+export * from "./components/primitives/types";
+
+export * from "./components/search";
+export * from "./hooks";
