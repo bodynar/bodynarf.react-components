93ffff7e42e245a6d39ada7f9a212d34881c2c1c
bodynar <bodynar@gmail.com>
2023-05-25 02:10:47 +0300

    add icon to anchor demo

--- test/src/pages/components/anchor/index.tsx
+++ test/src/pages/components/anchor/index.tsx
@@ -14,22 +14,33 @@ function Anchor() {
                     Default
                 </h4>
                 <code>
-                    {`<AnchorComponent caption="Link text" href="#" />`}
+                    {`<AnchorComponent caption={\`I"m the anchor\`} href="#" />`}
                 </code>
                 <br />
                 <br />
-                <AnchorComponent caption="Link text" href="#" />
+                <AnchorComponent caption={`I"m the anchor`} href="#" />
             </div>
             <div className="block">
                 <h4 className="subtitle is-5">
                     Without hover effects
                 </h4>
                 <code>
-                    {`<AnchorComponent caption="Link text" href="#" disableHovering={true} />`}
+                    {`<AnchorComponent caption={\`I"m the anchor\`} href="#" disableHovering={true} />`}
                 </code>
                 <br />
                 <br />
-                <AnchorComponent caption="Link text" href="#" disableHovering={true}/>
+                <AnchorComponent caption={`I"m the anchor`} href="#" disableHovering={true} />
+            </div>
+            <div className="block">
+                <h4 className="subtitle is-5">
+                    Icon content
+                </h4>
+                <code>
+                    {`<AnchorComponent caption={\`I"m the anchor\`} href="#" icon={{ name: "basket2", position: "right" }} />`}
+                </code>
+                <br />
+                <br />
+                <AnchorComponent caption={`I"m the anchor`} href="#" icon={{ name: "basket2", position: "right" }} />
             </div>
 
         </section>
