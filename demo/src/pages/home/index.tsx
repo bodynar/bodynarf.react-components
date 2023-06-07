1615e44972842f791eee133f9c607fa691b03871
bodynar <bodynar@gmail.com>
2023-06-07 22:55:58 +0300

    update home page

--- demo/src/pages/home/index.tsx
+++ demo/src/pages/home/index.tsx
@@ -1,22 +1,32 @@
-import { useState } from "react";
-
-import Button from "@bodynarf/react.components/components/button";
-
 /** Default page */
 function Home() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
-            <h1>Vite + React</h1>
-
-            <Button
-                type="primary"
-                caption={`count is ${count}`}
-                onClick={() => setCount((count) => count + 1)}
-            />
+            <h1 className="title is-1">
+                About
+            </h1>
+            <div className="block">
+                <div className="block">
+                    <h2 className="subtitle is-2">
+                        Components
+                    </h2>
+                    <p style={{ whiteSpace: "pre-line", fontStyle: "italic" }}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi minus pariatur ut molestias quibusdam nesciunt ipsum, ratione commodi tempore possimus, nobis repudiandae magnam rem. Ea adipisci dolorem sint doloremque voluptatibus.
+                    </p>
+                </div>
+                <div className="block">
+                    <h2 className="subtitle is-2">
+                        Controls
+                    </h2>
+                    <p style={{ whiteSpace: "pre-line" }}>
+                        Mostly all controls props type based on interface <code>BaseInputElementProps</code> which describes common structure.
+                        <br />
+                        And mostly all controls have implementation of this interface (like <code>size</code>, <code>style</code>, etc)
+                    </p>
+                </div>
+            </div>
         </>
-    )
+    );
 }
 
 export default Home;
