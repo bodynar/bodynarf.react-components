99292d3072e496f44108e1b428a659179df1bd52
bodynar <bodynar@gmail.com>
2024-02-27 00:20:56 +0300

    add common props demo pages

--- demo/src/pages/home/index.tsx
+++ demo/src/pages/home/index.tsx
@@ -2,26 +2,34 @@
 function Home() {
     return (
         <>
-            <h1 className="title is-1">
-                About
-            </h1>
+            <div className="block">
+                <h1 className="title is-1">
+                    About
+                </h1>
+            </div>
             <div className="block">
                 <div className="block">
                     <h2 className="subtitle is-2">
                         Components
                     </h2>
-                    <p style={{ whiteSpace: "pre-line", fontStyle: "italic" }}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi minus pariatur ut molestias quibusdam nesciunt ipsum, ratione commodi tempore possimus, nobis repudiandae magnam rem. Ea adipisci dolorem sint doloremque voluptatibus.
+                    <p>
+                        Components built via combining simple components or represent complex logical component
                     </p>
                 </div>
                 <div className="block">
                     <h2 className="subtitle is-2">
                         Controls
                     </h2>
-                    <p style={{ whiteSpace: "pre-line" }}>
-                        Mostly all controls props type based on interface <code>BaseInputElementProps</code> which describes common structure.
-                        <br />
-                        And mostly all controls have implementation of this interface (like <code>size</code>, <code>style</code>, etc)
+                    <p>
+                        Mostly all controls props type based on interface <code>BaseInputElementProps</code> which describes common structure
+                    </p>
+                </div>
+                <div className="block">
+                    <h2 className="subtitle is-2">
+                        Common props
+                    </h2>
+                    <p>
+                        Some props of base interface <code>BaseInputElementProps</code>
                     </p>
                 </div>
             </div>
