4f3c5fe7247d0981d5f0de8c5e2de7f7c13598cd
bodynar <bodynar@gmail.com>
2025-07-03 04:19:28 +0300

    v1.12 add file prop

--- src/components/file/types.ts
+++ src/components/file/types.ts
@@ -11,6 +11,12 @@ export type FileUploadProps = BaseElementProps & Pick<
     placeholder: string;
 
     /**
+     * File types\extensions allowed to select via component
+     * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Attributes/accept
+     */
+    accept?: string;
+
+    /**
      * Position of a upload file
      *
      * ! NOTE: Right alignment cannot be used with boxed style
