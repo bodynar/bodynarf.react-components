4d51c8de77070c728bbfac1a3156c573825b46d5
bodynar <bodynar@gmail.com>
2025-08-19 05:25:29 +0300

    move shared component, update home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,38 +1,123 @@
+import { useCallback } from "react";
+import CodeExample from "@app/sharedComponents/codeExample";
+
 /** Default page */
 function Home() {
+    const onLinkClick = useCallback(
+        (ref = "") => navigator.clipboard.writeText(`${window.location.origin}${window.location.pathname}#${ref}`),
+        []
+    );
+
     return (
         <>
-            <div className="block">
-                <h1 className="title is-1">
-                    About
-                </h1>
+            <div className="block" id="about">
+                <a onClick={() => onLinkClick("about")}>
+                    <h1 className="title is-1">
+                        About
+                    </h1>
+                </a>
+                <p>
+                    <code>@bodynarf/react.components</code> (also referred to as <code>BBR.Components</code>) is a library of ready-to-use React components styled with the Bulma CSS framework.
+                    <br />
+                    <br />
+                    Currently, the library is based on Bulma versions <code>&lt; 1.0.0</code>, although this is not explicitly listed in the dependencies. Versions <code>&gt; 1.0.0</code> can also be used, but you may encounter minor conflicts that can be easily fixed with simple style overrides.
+                    <br />
+                    <br />
+                    Each component imported from the library comes with a CSS class prefixed with <code>bbr</code> (<span className="is-italic">for example <code>.bbr-icon</code></span>).
+                    <br />
+                    <br />
+                    A grouped overview of all public components is provided below.
+                </p>
             </div>
-            <div className="block">
-                <div className="block">
+
+            <div className="block" id="install">
+                <a onClick={() => onLinkClick("install")}>
                     <h2 className="subtitle is-2">
+                        Installation & usage
+                    </h2>
+                </a>
+                <span>
+                    Before installing, make sure the following dependencies are already present:
+                </span>
+                <ul style={{ listStyle: "circle", paddingLeft: "2.5rem", marginBottom: "1rem" }}>
+                    <li>
+                        <a title="View the '@bodynarf/utils' package on npm" target="_blank" href="https://www.npmjs.com/package/@bodynarf/utils">
+                            @bodynarf/utils
+                        </a>
+                    </li>
+                    <li>
+                        <a title="View the 'bulma' package on npm" target="_blank" href="https://www.npmjs.com/package/bulma">
+                            bulma
+                        </a>
+                    </li>
+                    <li>
+                        <a title="View the 'react' package on npm" target="_blank" href="https://www.npmjs.com/package/react">
+                            react
+                        </a>
+                    </li>
+                </ul>
+                <span>
+                    You can install the component library by running the following command with npm:
+                </span>
+
+                <CodeExample code="npm install @bodynarf/react.components" language="sh" />
+
+                <span>
+                    Once installed, you can start using the components in your project. For example:
+                </span>
+                <CodeExample
+                    code={[
+                        `import ButtonComponent from "@bodynarf/react.components/components/button";`,
+                        "",
+                        "/* ... */",
+                        "",
+                        `<ButtonComponent type="success" caption="Button caption" />`
+                    ].join("\n")}
+                    language="tsx"
+                />
+                <span>
+                    A detailed description of each component can be found on its documentation page.
+                </span>
+            </div>
+
+            <div className="block">
+                <div className="block" id="contents">
+                    <a onClick={() => onLinkClick("contents")}>
+                        <h2 className="subtitle is-2">
+                            Contents
+                        </h2>
+                    </a>
+                </div>
+
+                <div className="block">
+                    <h3 className="subtitle is-3">
                         Components
-                    </h2>
+                    </h3>
                     <p>
-                        Components built via combining simple components or represent complex logical component
+                        Overview of the basic building blocks provided by the library.
                     </p>
                 </div>
+
                 <div className="block">
-                    <h2 className="subtitle is-2">
+                    <h3 className="subtitle is-3">
                         Controls
-                    </h2>
+                    </h3>
                     <p>
-                        Mostly all controls props type based on interface <code>BaseInputElementProps</code> which describes common structure
+                        Input elements and form controls designed for flexible use. Most control props are based on the <code>BaseInputElementProps</code> type, which defines common fields and simplifies configuration and interaction.
                     </p>
                 </div>
+
                 <div className="block">
-                    <h2 className="subtitle is-2">
+                    <h3 className="subtitle is-3">
                         Common props
-                    </h2>
+                    </h3>
                     <p>
-                        Some props of base interface <code>BaseInputElementProps</code>
+                        Shared properties available across multiple components for consistent customization. This section covers the <code>BaseInputElementProps</code> type in detail.
                     </p>
                 </div>
             </div>
+
+
         </>
     );
 }
