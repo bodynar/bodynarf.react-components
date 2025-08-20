48060cd5b2e47662c9d789ff850e88eaa5a47da8
bodynar <bodynar@gmail.com>
2025-08-20 04:22:00 +0300

    update anchor page

--- src/pages/components/anchor/index.tsx
+++ src/pages/components/anchor/index.tsx
@@ -1,34 +1,179 @@
-import { ElementPosition } from "@bodynarf/react.components";
+import { useCallback, useState } from "react";
+
+import { ElementPosition, ElementSize } from "@bodynarf/react.components";
 import AnchorComponent from "@bodynarf/react.components/components/anchor";
 
 import ComponentUseCase from "@app/sharedComponents/useCase";
-import CommonPropsSuppressExampleInfoMessage from "@app/sharedComponents/commonPropsSuppress";
 import DemoComponentTitleInfoMessage from "@app/sharedComponents/title";
+import CodeExample from "@app/sharedComponents/codeExample";
 
 /** Anchor component demo */
 function Anchor() {
+    const [text, setText] = useState("");
+    const appendText = useCallback(
+        () => setText(
+            t => t
+                + "\n"
+                + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getMilliseconds()
+                + " => " + "clicked"
+        ),
+        []
+    );
+
     return (
         <section>
-            <DemoComponentTitleInfoMessage name="Anchor" />
+            <DemoComponentTitleInfoMessage name="Anchor" hidePropsNotice />
+
             <ComponentUseCase
-                caption="Default"
-                code={`<Anchor caption={\`I"m the anchor\`} href="#" />`}
-                description="Default configuration is caption and href"
-                component={<AnchorComponent caption={`I"m the anchor`} href="#" />}
-            />
-            <CommonPropsSuppressExampleInfoMessage />
+                caption="Minimal use"
+                description="The minimal configuration requires either a caption or an icon"
+                code={<CodeExample
+                    code={[
+                        `import AnchorComponent from "@bodynarf/react.components/components/anchor";`,
+                        "",
+                        "/* ... */",
+                        "",
+                        '<AnchorComponent caption="Click me!" />',
+                    ].join("\n")}
+                />}
+            >
+                <AnchorComponent caption="Click me!" />
+            </ComponentUseCase>
+
+            <ComponentUseCase
+                caption="icon"
+                captionIsCode
+                description="The component can render an icon as its content"
+                code={<CodeExample
+                    code={[
+                        `import { ElementSize } from "@bodynarf/react.components";`,
+                        `import AnchorComponent from "@bodynarf/react.components/components/anchor";`,
+                        "",
+                        "/* ... */",
+                        "",
+                        '<AnchorComponent',
+                        '    icon={{ name: "basket2", size: ElementSize.Large }}',
+                        '/>',
+                    ].join("\n")}
+                />}
+            >
+                <AnchorComponent
+                    icon={{ name: "basket2", size: ElementSize.Large }}
+                />
+            </ComponentUseCase>
+
+            <ComponentUseCase
+                caption="Caption with icon"
+                description="The component can display both a caption and an icon together"
+                code={<CodeExample
+                    code={[
+                        `import { ElementPosition } from "@bodynarf/react.components";`,
+                        `import AnchorComponent from "@bodynarf/react.components/components/anchor";`,
+                        "",
+                        "/* ... */",
+                        "",
+                        '<AnchorComponent',
+                        '    caption="Click me!"',
+                        '    icon={{ name: "basket2", position: ElementPosition.Right }}',
+                        '/>',
+                    ].join("\n")}
+                />}
+            >
+                <AnchorComponent
+                    caption="Click me!"
+                    icon={{ name: "basket2", position: ElementPosition.Right }}
+                />
+            </ComponentUseCase>
+
             <ComponentUseCase
                 caption="Without hover effects"
-                code={`<Anchor disableHovering />`}
-                description="Hover effects could be disabled (link wont have underline effect on hover). But why?.."
-                component={<AnchorComponent caption={`I"m the anchor`} href="#" disableHovering />}
-            />
+                description="Hover effects can be disabled (the link will not be underlined on hover). But why would you?.."
+                code={<CodeExample
+                    code={[
+                        `import AnchorComponent from "@bodynarf/react.components/components/anchor";`,
+                        "",
+                        "/* ... */",
+                        "",
+                        '<AnchorComponent caption="Click me!" disableHovering />',
+                    ].join("\n")}
+                />}
+            >
+                <AnchorComponent caption="Click me!" disableHovering />
+            </ComponentUseCase>
+
             <ComponentUseCase
-                caption="With icon"
-                code={`<Anchor icon={{ name: "basket2", position: ElementPosition.Right }} />`}
-                description="Component could held an icon in its content"
-                component={<AnchorComponent caption={`I"m the anchor`} href="#" icon={{ name: "basket2", position: ElementPosition.Right }} />}
-            />
+                caption="href"
+                captionIsCode
+                description="You can provide an href to make the anchor navigate to a specific link when clicked"
+                code={<CodeExample
+                    code={[
+                        `import AnchorComponent from "@bodynarf/react.components/components/anchor";`,
+                        "",
+                        "/* ... */",
+                        "",
+                        '<AnchorComponent',
+                        '    caption="Open npm main page"',
+                        '    href="https://www.npmjs.com/"',
+                        '/>',
+                    ].join("\n")}
+                />}
+            >
+                <AnchorComponent
+                    caption="Open npm main page"
+                    href="https://www.npmjs.com/"
+                />
+            </ComponentUseCase>
+
+            <ComponentUseCase
+                caption="target"
+                captionIsCode
+                description="The target attribute defines where the linked document will open (for example, in a new tab)"
+                code={<CodeExample
+                    code={[
+                        `import AnchorComponent from "@bodynarf/react.components/components/anchor";`,
+                        "",
+                        "/* ... */",
+                        "",
+                        '<AnchorComponent',
+                        '    target="_blank"',
+                        '    href="https://www.npmjs.com/"',
+                        '    caption="Open npm main page in new tab"',
+                        '/>',
+                    ].join("\n")}
+                />}
+            >
+                <AnchorComponent
+                    target="_blank"
+                    href="https://www.npmjs.com/"
+                    caption="Open npm main page in new tab"
+                />
+            </ComponentUseCase>
+
+            <ComponentUseCase
+                captionIsCode
+                caption="onClick"
+                description="Additional click handler for the anchor"
+                code={<CodeExample
+                    code={[
+                        `import AnchorComponent from "@bodynarf/react.components/components/anchor";`,
+                        "",
+                        "/* ... */",
+                        "",
+                        '<AnchorComponent',
+                        '    caption="Click me!"',
+                        '    onClick={appendText}',
+                        '/>',
+                    ].join("\n")}
+                />}
+            >
+                <AnchorComponent
+                    caption="Click me!"
+                    onClick={appendText}
+                />
+                <p style={{ whiteSpace: "pre-line" }}>
+                    {text}
+                </p>
+            </ComponentUseCase>
         </section>
     );
 }
