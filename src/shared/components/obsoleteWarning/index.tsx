4c30f0062355e51dc81d53f81fa622fa7254cf55
bodynar <bodynar@gmail.com>
2025-08-20 04:36:19 +0300

    add shared component

---
+++ src/shared/components/obsoleteWarning/index.tsx
@@ -0,0 +1,36 @@
+import { FC, ReactNode } from "react";
+
+import Icon from "@bodynarf/react.components/components/icon";
+
+/* Props type of `ObsoleteWarning` */
+type ObsoleteWarningProps = {
+    /** Version when deprecated mark appears */
+    version: string;
+
+    /** Recommendation about code update */
+    recommendation: ReactNode;
+
+    /**
+     * Current deprecation state
+     * @default "warning"
+     */
+    state?: "warning" | "danger";
+};
+
+/* Obsolete warning panel */
+const ObsoleteWarning: FC<ObsoleteWarningProps> = ({
+    version, recommendation, state = "warning"
+}) => {
+    return (
+        <article className={`message is-${state}`}>
+            <div className="message-body">
+                <Icon name="exclamation-triangle-fill" /> Warning!
+                This component has been marked as obsolete since version <code>{version}</code>
+                <br />
+                {recommendation}
+            </div>
+        </article>
+    );
+};
+
+export default ObsoleteWarning;
