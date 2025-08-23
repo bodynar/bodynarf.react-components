19491d4eaab5d3d46a3968570c991b90debc4eb4
bodynar <bodynar@gmail.com>
2025-08-23 15:10:04 +0300

    deprecate anchor

--- src/components/anchor/component/index.tsx
+++ src/components/anchor/component/index.tsx
@@ -6,7 +6,10 @@ import { AnchorProps } from "..";
 import SimpleAnchor from "../components/simpleAnchor";
 import AnchorWithIcon from "../components/anchorWithIcon";
 
-/** Anchor component */
+/**
+ * Anchor component
+ * @deprecated Since v1.12.7. Use html element with custom formatting
+ */
 export default function Anchor(props: AnchorProps): JSX.Element {
     if (isNullOrUndefined(props.caption) && isNullOrUndefined(props.icon)) {
         throw new Error("No anchor content provided");
