5bc048d24d149999a172ba66464a17d9595c4907
bodynar <bodynar@gmail.com>
2025-08-26 04:56:49 +0300

    add tag demo

--- src/pages/components/tag/index.tsx
+++ src/pages/components/tag/index.tsx
@@ -1,77 +1,236 @@
-import { ElementColor, ElementSize } from "@bodynarf/react.components";
+import { FC, useCallback, useState } from "react";
+
+import { ElementColor, ElementSize, SelectableItem } from "@bodynarf/react.components";
 import TagComponent from "@bodynarf/react.components/components/tag";
 
 import ComponentUseCase from "@app/sharedComponents/useCase";
-import CommonPropsSuppressExampleInfoMessage from "@app/sharedComponents/commonPropsSuppress";
-import ComponentSizeCase from "@app/sharedComponents/sizeUse";
 import ComponentColorCase from "@app/sharedComponents/colorUse";
 import DemoComponentTitleInfoMessage from "@app/sharedComponents/title";
+import CodeExample from "@app/sharedComponents/codeExample";
+import ComponentEnumCase from "@app/sharedComponents/enumSelectionCase";
+
+const sizes: Array<string> =
+    Object
+        .keys(ElementSize)
+        .filter(x => x !== "Small");
+
+const sizesAsSelectList: Array<SelectableItem> =
+    Object
+        .values(ElementSize)
+        .filter(x => x !== ElementSize.Small)
+        .map((x, i) => ({
+            displayValue: x,
+            id: i.toString(),
+            value: x,
+        }) as SelectableItem);
 
 /** Tag component demo */
-function Tag() {
+const Tag: FC = () => {
+    const [clickLog, setClickLog] = useState("");
+    const appendClickLog = useCallback(
+        () => setClickLog(
+            t => t
+                + "\n"
+                + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getMilliseconds()
+                + " => " + "clicked"
+        ),
+        []
+    );
+
     return (
         <section>
-            <DemoComponentTitleInfoMessage name="Tag" />
-            <ComponentUseCase
-                caption="Default"
-                code={`<Tag content="Tag content" />`}
-                description="By default its only content required"
-                component={<TagComponent content="Tag content" />}
+            <DemoComponentTitleInfoMessage
+                name="Tag"
+                hidePropsNotice
+                description="A component for displaying information about a category \ tag"
             />
-            <CommonPropsSuppressExampleInfoMessage />
+
             <ComponentUseCase
-                caption="Rounded"
+                caption="Minimal use"
+                description="To use the component, you only need to provide the text content"
+                code={
+                    <CodeExample
+                        code={[
+                            `import TagComponent from "@bodynarf/react.components/components/tag";`,
+                            "",
+                            "/* ... */",
+                            "",
+                            '<TagComponent content="Minimal use" />',
+                        ].join("\n")}
+                    />
+                }
+            >
+                <TagComponent content="Minimal use" />
+            </ComponentUseCase>
+
+            <ComponentEnumCase
                 captionIsCode
-                code={`<Tag rounded />`}
-                description="Makes corners more rounded"
-                component={<TagComponent content="Tag content" style={ElementColor.Success} rounded />}
-            />
-            <ComponentSizeCase
-                caption="Sizes"
-                codeProvider={id => `<Tag size={ElementSize.${id}} />`}
-                description="Component supports all available sizes. But normal and small size are equal"
+                caption="size"
+                enumNames={sizes}
+                lookupValues={sizesAsSelectList}
+                description="The component supports all sizes defined in ElementSize, except Small"
+                codeProvider={id =>
+                    <CodeExample
+                        code={[
+                            `import { ElementSize } from "@bodynarf/react.components";`,
+                            `import TagComponent from "@bodynarf/react.components/components/tag";`,
+                            "",
+                            "/* ... */",
+                            "",
+                            '<TagComponent',
+                            '    content="Size tag"',
+                            `    size={ElementSize.${id}}`,
+                            '/>',
+                        ].join("\n")}
+                    />
+                }
                 componentProvider={
-                    size =>
+                    (value: Exclude<ElementSize, ElementSize.Small>) =>
                         <TagComponent
-                            content="Tag content"
-                            size={size === ElementSize.Small ? ElementSize.Normal : size}
-                            style={ElementColor.Success}
+                            content="Size tag"
+                            size={value}
                         />
                 }
             />
+
             <ComponentColorCase
-                caption="Colors"
-                codeProvider={id => `<Tag style={ElementColor.${id}} />`}
+                captionIsCode
+                caption="style"
                 description="Component supports all available colors"
+                codeProvider={id =>
+                    <CodeExample
+                        code={[
+                            `import { ElementColor } from "@bodynarf/react.components";`,
+                            `import TagComponent from "@bodynarf/react.components/components/tag";`,
+                            "",
+                            "/* ... */",
+                            "",
+                            '<TagComponent',
+                            '    content="Style tag"',
+                            `    style={ElementColor.${id}}`,
+                            '/>',
+                        ].join("\n")}
+                    />
+                }
                 componentProvider={
                     style =>
                         <TagComponent
-                            content="Tag content"
                             style={style}
+                            content="Style tag"
                         />
                 }
             />
+
             <ComponentUseCase
-                caption="Custom color"
-                code={`<Tag customColor={{ color: "black", backgroundColor: "#98ffff" }} />`}
-                description={`Besides of pre-defined styled it"s possible to configure with custom colors`}
-                component={<TagComponent content="Custom color" customColor={{ color: "black", backgroundColor: "#98ffff" }} />}
-            />
-            <ComponentColorCase
-                caption="Colors (light)"
-                codeProvider={id => `<Tag style={ElementColor.${id}} lightColor />`}
-                description={<>Colors could be made lighter by passing <code>lightColor</code> prop</>}
-                componentProvider={
-                    style =>
-                        <TagComponent
-                            content="Tag content"
-                            style={style}
-                            lightColor
-                        />
+                captionIsCode
+                caption="rounded"
+                description="Option for displaying the component with rounded corners. Disabled by default"
+                code={
+                    <CodeExample
+                        code={[
+                            `import TagComponent from "@bodynarf/react.components/components/tag";`,
+                            "",
+                            "/* ... */",
+                            "",
+                            '<TagComponent',
+                            '    rounded',
+                            '    content="Rounded use"',
+                            '/>',
+                        ].join("\n")}
+                    />
                 }
-            />
+            >
+                <TagComponent
+                    rounded
+                    content="Rounded use"
+                />
+            </ComponentUseCase>
+
+            <ComponentUseCase
+                captionIsCode
+                caption="lightColor"
+                description="Option for displaying light shades of the style. Disabled by default"
+                code={
+                    <CodeExample
+                        code={[
+                            `import TagComponent from "@bodynarf/react.components/components/tag";`,
+                            "",
+                            "/* ... */",
+                            "",
+                            '<TagComponent',
+                            '    lightColor',
+                            '    content="LightColor use"',
+                            '    style={ElementColor.Primary}',
+                            '/>',
+                        ].join("\n")}
+                    />
+                }
+            >
+                <TagComponent
+                    lightColor
+                    content="LightColor use"
+                    style={ElementColor.Primary}
+                />
+            </ComponentUseCase>
+
+            <ComponentUseCase
+                captionIsCode
+                caption="customColor"
+                description="Option to set custom component colors, only CSS-valid values can be used. Empty by default"
+                code={
+                    <CodeExample
+                        code={[
+                            `import TagComponent from "@bodynarf/react.components/components/tag";`,
+                            "",
+                            "/* ... */",
+                            "",
+                            '<TagComponent',
+                            '    content="CustomColor use"',
+                            '    customColor={{ backgroundColor: "#76dffb", color: "white", }}',
+                            '/>',
+                        ].join("\n")}
+                    />
+                }
+            >
+                <TagComponent
+                    content="CustomColor use"
+                    customColor={{ backgroundColor: "#76dffb", color: "white", }}
+                />
+            </ComponentUseCase>
+
+            <ComponentUseCase
+                captionIsCode
+                caption="onClick"
+                description="Function for handling click events on the component"
+                code={
+                    <CodeExample
+                        code={[
+                            `import { useCallback } from "react"`,
+                            "",
+                            `import TagComponent from "@bodynarf/react.components/components/tag";`,
+                            "",
+                            "/* ... */",
+                            "const ON_CLICK_HANDLE_FN = useCallback(() => { /* handler fn */}, []);",
+                            "/* ... */",
+                            "",
+                            '<TagComponent',
+                            '    content="onClick use"',
+                            '    onClick={ON_CLICK_HANDLE_FN}',
+                            '/>',
+                        ].join("\n")}
+                    />
+                }
+            >
+                <TagComponent
+                    content="LightColor use"
+                    onClick={appendClickLog}
+                />
+                <p style={{ whiteSpace: "pre-line" }}>
+                    {clickLog}
+                </p>
+            </ComponentUseCase>
         </section>
     );
-}
+};
 
 export default Tag;
