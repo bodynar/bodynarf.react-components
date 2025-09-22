01f21e4ac730be45e1e70d0b93f2d6a8f20c8f15
bodynar <bodynar@gmail.com>
2025-09-23 02:46:12 +0300

    extend re-export file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,64 @@
+export { default as Accordion } from "./accordion";
 export * from "./accordion";
+
+export { default as Anchor } from "./anchor";
 export * from "./anchor";
+
+export { default as Breadcrumbs } from "./breadcrumbs";
+export * from "./breadcrumbs";
+
+export { default as Button } from "./button";
 export * from "./button";
+
+export { default as Dropdown } from "./dropdown";
 export * from "./dropdown";
+
+export { default as File } from "./file";
+export * from "./file";
+
+export { default as Icon } from "./icon";
 export * from "./icon";
+
+export { default as Multiselect } from "./multiselect";
+export * from "./multiselect";
+
+export { default as Paginator } from "./paginator";
 export * from "./paginator";
+
+export { default as Search } from "./search";
 export * from "./search";
+
+export { default as Table } from "./table";
+export * from "./table";
+
+export { default as Tabs } from "./tabs";
 export * from "./tabs";
+
+export { default as Tag } from "./tag";
 export * from "./tag";
 
-export * from "./primitives";
+// Progress/Loader component
+export { default as Progress } from "./progress";
+export * from "./progress";
+
+// Primitives
+export { default as Checkbox } from "./primitives/checkbox";
+export * from "./primitives/checkbox";
+
+export { default as Color } from "./primitives/color";
+export * from "./primitives/color";
+
+export { default as Date } from "./primitives/date";
+export * from "./primitives/date";
+
+export { default as Multiline } from "./primitives/multiline";
+export * from "./primitives/multiline";
+
+export { default as NumberInput } from "./primitives/number";
+export * from "./primitives/number";
+
+export { default as Password } from "./primitives/password";
+export * from "./primitives/password";
+
+export { default as Text } from "./primitives/text";
+export * from "./primitives/text";
