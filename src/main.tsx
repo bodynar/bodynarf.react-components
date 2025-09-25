74813805cf77fc53d6c97312f93549ec4ddd94ce
bodynar <bodynar@gmail.com>
2025-09-26 01:44:34 +0300

    use another router

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 
 import App from "./component";
 
-import "./style.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bulma/bulma.sass";
 import "bulma-checkradio/dist/css/bulma-checkradio.min.css";
+
+import "./style.scss";
 
 createRoot(
 	document.getElementById("root") as HTMLElement
 ).render(
 	<StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</StrictMode>
 );
 
